763051b3cc83880de658e9b47b73b4ca3f776e57
danil <shershnev.danil2000@gmail.com>
2020-11-14 15:34:34 +0300

    SER-4 patch added interface spec

--- ServicesApp.Core/Interfaces/IRepository.cs
+++ ServicesApp.Core/Interfaces/IRepository.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using ServicesApp.Core.Entities;
+
 namespace ServicesApp.Core.Interfaces
 {
     public interface IRepository<T> where T : BaseEntity
     {
+        IAsyncEnumerable<T> GetAll();
+
+        Task<T> GetById(int id);
+
+        Task Add(T entity);
+
+        void Update(T entity);
+
+        Task Delete(int id);
     }
 }
