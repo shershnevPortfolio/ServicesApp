85bd08504acc7e70c0071a3217b250be5286e819
danil <shershnev.danil2000@gmail.com>
2020-11-14 13:50:41 +0300

    SER-4 done

---
+++ ServicesApp.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesApp.Core.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        void SaveChanges();
+
+        Task SaveChangesAsync();
+    }
+}
