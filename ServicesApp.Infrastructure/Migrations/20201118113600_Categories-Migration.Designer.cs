933e7fd73453acf767c98a8c89ebe66b032736a1
danil <shershnev.danil2000@gmail.com>
2020-11-18 14:37:36 +0300

    SER-5 --patch added category migrations

--- ServicesApp.Infrastructure/Migrations/20201118113600_Categories-Migration.Designer.cs
+++ ServicesApp.Infrastructure/Migrations/20201118113600_Categories-Migration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServicesApp.Infrastructure.Data;
 
 namespace ServicesApp.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20201118113600_Categories-Migration")]
+    partial class CategoriesMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
