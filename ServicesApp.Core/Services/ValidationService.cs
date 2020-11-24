03454b34d7c8d84fc81ce995cfd9131de3b43135
danil <shershnev.danil2000@gmail.com>
2020-11-24 19:28:26 +0300

    SER-12 done

---
+++ ServicesApp.Core/Services/ValidationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ServicesApp.Core.Interfaces;
+
+namespace ServicesApp.Core.Services
+{
+    class ValidationService : IValidationService
+    {
+        public void ValidateQuery<T>(T Query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidateQueryResult<T>(T result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
